b0d2ec897b4484098c1ece4ea16c026fd0438660
moaberglund <moa.21@hotmail.com>
2025-02-18 15:58:14 +0100

    clean

--- BookStore/Controllers/StatusController.cs
+++ BookStore/Controllers/StatusController.cs
@@ -51,8 +51,6 @@ namespace BookStore.Controllers
         }
 
         // POST: Status/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,ReadingStatus")] Status status)
@@ -83,8 +81,6 @@ namespace BookStore.Controllers
         }
 
         // POST: Status/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,ReadingStatus")] Status status)
